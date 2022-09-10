6883d0b1b3f0715a98d43b9e703103e82b04c94b
andres-echeverri-dev <andres.echeverri@on3d.com.co>
2022-09-10 18:45:06 -0500

    Primer commit

---
+++ Source/UETC_Proyecto_Final/UETC_Proyecto_FinalGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UETC_Proyecto_FinalGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUETC_Proyecto_FinalGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUETC_Proyecto_FinalGameMode();
+};
+
+
+
